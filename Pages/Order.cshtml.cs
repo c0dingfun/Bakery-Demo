b3ae46a58b45665c45621628d7eea1a083cfd5e5
c0dingfun <hackerrank888@gmail.com>
2020-03-18 12:36:12 -0700

    added order page and validation

--- Pages/Order.cshtml.cs
+++ Pages/Order.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Bakery.Data;
@@ -14,7 +15,7 @@ namespace Bakery.Pages
         // public void OnGet() { }
 
         private BakeryContext db;
-        public OrderModel(BakeryContext db)=> this.db = db;
+        public OrderModel(BakeryContext db) => this.db = db;
 
         // HH: This attribute ensures that the property is included in the **model 
         // binding** process, which results in values passed as part of an HTTP request 
@@ -25,7 +26,7 @@ namespace Bakery.Pages
         // The Order page is reached by clicking a link on the home page, which results in 
         // a GET request. You must add SupportsGet = true to opt in to model binding on 
         // GET requests.
-        [BindProperty(SupportsGet=true)]
+        [BindProperty(SupportsGet = true)]
 
         // route-parameter: order/3
         // query string order?id=3
@@ -33,6 +34,25 @@ namespace Bakery.Pages
         public int Id { get; set; }
         public Product Product { get; set; }
 
-        public async Task OnGetAsync()=> Product=await db.Products.FindAsync(Id);
+        [BindProperty, EmailAddress, Required, Display(Name = "Your Email Address")]
+        public string OrderEmail { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "Please Supply a shipping address"), Display(Name = "Shipping Address")]
+        public string OrderShipping { get; set; }
+
+        [BindProperty, Display(Name = "Quantity")]
+        public int OrderQuantity { get; set; } = 1;
+
+        public async Task OnGetAsync() => Product = await db.Products.FindAsync(Id);
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Product = await db.Products.FindAsync(Id);
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("OrderSuccess");
+            }
+            return Page();
+        }
     }
 }
